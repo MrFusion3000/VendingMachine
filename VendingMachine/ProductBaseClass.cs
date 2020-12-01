9007156fa90ef9727f1c5272a1ef9a3de9706cd1
MrFusion3000 <nico@crepro.com>
2020-12-01 15:40:55 +0100

    VendingMachine V0.02 start of abstract classes

---
+++ VendingMachine/ProductBaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public abstract class ProductBaseClass
+    {
+        public int ProductId { get; }
+        public string ProductName { get; }
+
+        public int Price { get; }
+
+        public int AmountInStock { get; set; }
+
+        public bool OutOfStock { get; set; }
+    }
+}
---
+++ VendingMachine/VendingaMachineBaseClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public abstract class VendingMachineBaseClass
+    {
+        //List of products, array of products or list of enums
+        public int CustomerWallet { get; set; }
+        public int ShoppingCartWallet { get; set; }
+        public int MyProperty { get; set; }
+
+        public abstract void AddToCustomerWallet();
+
+        public abstract void SubFromCustomerWallet();
+
+        public abstract void AddToShoppingCartWallet();
+
+        public abstract void SubFromShoppingCartWallet();
+
+
+
+    }
+
+
+}
